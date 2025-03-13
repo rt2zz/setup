10d929c7c0bebf0318241d27392c3dcd87936726
Zack Story <ztstory@gmail.com>
2025-03-13 11:41:04 -0700

    auto-commit 2025-03-13 11:41:04

--- sync.sh
+++ sync.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+
 git add -A
 git commit -m "auto-commit $(date +"%Y-%m-%d %H:%M:%S")"
+
 
 # Try to push and capture the exit status
 if ! git push origin main; then
     echo "Push failed, attempting to show notification..."
-    osascript -e "display alert \"Repo Sync Failed\" message \"Please check directory: $(pwd)\""
+    osascript -e "display alert \"Repo Sync Failed\" message \"Please check directory: $(DIR)\""
 fi
 
